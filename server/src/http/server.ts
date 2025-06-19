7f305e271e62199db0c29494179b06c50147643e
Pavwos <rspavanelli@gmail.com>
2025-06-19 20:13:22 -0300

    Web && Server II

---
+++ server/src/http/server.ts
@@ -0,0 +1,32 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goals'
+import { getPedingGoalsRoute } from './routes/create-pending-goals'
+import { createCompletionRoute } from './routes/create-completion'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPedingGoalsRoute)
+app.register(getWeekSummaryRoute)
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running...') //That's much too vulgar a display of power, Karras
+  })

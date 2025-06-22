3cd64c0b7fc477d2b4046ba04f3930a5919db6e3
Pavwos <rspavanelli@gmail.com>
2025-06-22 20:51:57 -0300

    package fix

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -28,6 +28,7 @@ const PORT = Number(process.env.PORT) || 3333
 app
   .listen({
     port: PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log(`HTTP Server Running on port ${PORT}`)
